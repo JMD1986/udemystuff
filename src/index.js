e4bd70766a19a70cb862dbde7c759b22e1bd381a
John Daugherty <johndaugherty1986@gmail.com>
2017-12-14 23:47:23 -0600

    post component in route

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
@@ -14,7 +15,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
 
